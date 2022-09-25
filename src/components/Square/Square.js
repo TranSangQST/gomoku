f7cc361d2e0978db8868668776d24cec4399576f
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2022-09-25 18:50:10 +0700

    config deploy

---
+++ src/components/Square/Square.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from "react";
+
+import "./Square.css"
+
+
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+        {props.value}
+        </button>
+    );
+}
+
+export default Square;
